4a6e19ae5a37647e5872bc5647abe381210bf8ca
Dave Tickle <dave.tickle@socialfinance.org.uk>
2023-04-15 19:32:47 +0100

    profile control block and additional header buttons

--- js/src/components/mainmenu/index.ts
+++ js/src/components/mainmenu/index.ts
@@ -1,18 +1,19 @@
-import { render, BindingProps } from "dom-component-helper";
+import { render, BindingProps } from 'dom-component-helper';
 
 const MainMenu = (props: BindingProps) => {
   const { domElement } = props;
 
-  const button = domElement.querySelector("a");
-  const container = domElement.querySelector(".hef-menu__container");
+  const button = domElement.querySelector('[data-trigger]');
+  console.log(button);
+  const container = domElement.querySelector('.hef-menu__container');
 
-  button.addEventListener("click", (evt: Event) => {
+  button.addEventListener('click', (evt: Event) => {
     evt.preventDefault();
 
-    container.classList.toggle("hef-menu__container--open");
+    container.classList.toggle('hef-menu__container--open');
 
-    button.querySelectorAll("span").forEach((el: Element) => {
-      el.classList.toggle("hef-menu__link-text--hidden");
+    button.querySelectorAll('span').forEach((el: Element) => {
+      el.classList.toggle('hef-menu__link-text--hidden');
     });
   });
 };
