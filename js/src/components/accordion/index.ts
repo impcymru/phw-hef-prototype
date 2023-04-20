c74ad6e8779c80ea54c52c896667a611857ac968
Dave Tickle <dave.tickle@socialfinance.org.uk>
2023-04-20 10:50:00 +0100

    adding accordion items

---
+++ js/src/components/accordion/index.ts
@@ -0,0 +1,43 @@
+import { render, BindingProps } from 'dom-component-helper';
+import * as nunjucks from 'nunjucks';
+import template from './accordion.html';
+
+const Accordion = (props: BindingProps) => {
+  const component = document.createElement('div');
+
+  const data = JSON.parse(props.data);
+
+  const content = nunjucks.renderString(template, data);
+
+  component.innerHTML = content;
+
+  const output = render(props.domElement, component);
+
+  const parent = output.querySelector('[data-trigger]');
+
+  parent.addEventListener('click', (evt: Event) => {
+    const el = evt.target as Element;
+    const parent = el
+      .closest('.hef-expander')
+      .classList.toggle('hef-expander--closed');
+  });
+
+  output.querySelectorAll('[data-sub-trigger]').forEach((el) => {
+    el.addEventListener('click', (evt: Event) => {
+      const el = evt.target as Element;
+      const parent = el
+        .closest('.hef-sub-expander')
+        .classList.toggle('hef-sub-expander--closed');
+    });
+  });
+
+  output.querySelectorAll('[data-radio]').forEach((radio) => {
+    radio.addEventListener('change', () => {
+      parent
+        .querySelector('.check-icon')
+        .classList.remove('check-icon--hidden');
+    });
+  });
+};
+
+export default Accordion;
